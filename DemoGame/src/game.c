5ebe6a5fc8d11315e675c34dc9be83fc1467c0a8
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2024-09-24 23:45:37 +1000

    just add this as a macro

--- DemoGame/src/game.c
+++ DemoGame/src/game.c
@@ -11,7 +11,7 @@ gameservices_t *GetMServices(mservices_t *services)
 	gameservices = (gameservices_t)		// ensure all fields are initialized
 	{
 		.gamename = "DemoGame",			// name of the game used by the engine for windowing
-		.version = MENGINE_VERSION,		// this field is for the verion of the engine that the game was built for
+		.version = 1,					// this field is for the verion of the engine that the game was built for
 		.iconpath = "",					// path to the icon file for the game
 		.smiconpath = "",				// path to the small icon file for the game
 	};
