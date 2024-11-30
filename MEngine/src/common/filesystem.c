9202c41695f52547f4d8930518c0c59523b85197
Marco Grimaldi <marco.grimaldi20@outlook.com>
2024-11-30 13:09:38 +1100

    stops this thing complaining

--- MEngine/src/common/filesystem.c
+++ MEngine/src/common/filesystem.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "common.h"
 #include "unzip.h"
 
@@ -8,4 +10,40 @@ bool FileSys_Init(void)
 
 void FileSys_Shutdown(void)
 {
+}
+
+bool FileSys_FileExists(const char *filename)
+{
+	FILE *file = fopen(filename, "r");
+	if (!file)
+		return(false);
+
+	fclose(file);
+	return(true);
+}
+
+size_t FileSys_FileSize(const char *filename)
+{
+	return(0);
+}
+
+time_t FileSys_FileTimeStamp(const char *filename)
+{
+	return(0);
+}
+
+unsigned int FileSys_CountFiles(const char *directory, const char *filter)
+{
+	return(0);
+}
+
+filedata_t *FileSys_ListFiles(const char *directory, const char *filter, unsigned int numfiles)		// if numfiles is 0, all files will be listed
+{
+	return(NULL);
+}
+
+void FileSys_FreeFileList(filedata_t *filelist)
+{
+	free(filelist);
+	filelist = NULL;
 }
