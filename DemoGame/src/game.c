d4acc74ace984200554852d795e89d895822a9cf
emcapsulation <19515620+emcapsulation@users.noreply.github.com>
2024-12-01 11:48:58 +1100

    a new cvar joins the crew

--- DemoGame/src/game.c
+++ DemoGame/src/game.c
@@ -1,11 +1,22 @@
 #include <stddef.h>
 #include "../../MEngine/src/mservices.h"
 
+#include <stdio.h>
+
 mservices_t mservices;
 gameservices_t gameservices;
 
 static bool Init(void)
 {
+	(cvar_t *)mservices.cvarsystem->RegisterString("mycvar", "test", CVAR_GAME | CVAR_ARCHIVE, "this is my epic cvar");
+	cvar_t* hello = mservices.cvarsystem->Find("mycvar");
+	if (hello == NULL) {
+		printf("%s\n", "Its null");
+	} else {
+		printf("%s\n", mservices.cvarsystem->GetString(hello));
+		mservices.log->WriteSeq(LOG_INFO, "%s", mservices.cvarsystem->GetString(hello));
+	}
+	
 	return(true);
 }
 
@@ -14,7 +25,7 @@ static void Shutdown(void)
 }
 
 static void RunFrame(void)
-{
+{	
 }
 
 gameservices_t *GetMServices(mservices_t *services)
