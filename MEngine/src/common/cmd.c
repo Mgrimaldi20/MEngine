f6cdd7d0e1c4d63fee02e8f347589f5dc4b9cb3b
Marco Grimaldi <marco.grimaldi20@outlook.com>
2025-01-05 10:55:51 +1100

    just void args and cast a big struct for args, easy

--- MEngine/src/common/cmd.c
+++ MEngine/src/common/cmd.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #define DEF_CMD_MAP_CAPACITY 256
+#define DEF_CMD_BUFFER_SIZE 1024
 
 typedef struct
 {
@@ -23,16 +24,78 @@ typedef struct
 } cmdmap_t;
 
 static cmdmap_t *cmdmap;
+static char *cmdbuffer;
+
+static bool initialized;
 
 bool Cmd_Init(void)
 {
+	if (initialized)
+		return(true);
+
+	cmdbuffer = MemCache_Alloc(sizeof(*cmdbuffer) * DEF_CMD_BUFFER_SIZE);
+	if (!cmdbuffer)
+	{
+		Log_WriteSeq(LOG_ERROR, "Failed to allocate memory for command buffer");
+		return(false);
+	}
+
+	cmdmap = MemCache_Alloc(sizeof(*cmdmap));
+	if (!cmdmap)
+	{
+		Log_WriteSeq(LOG_ERROR, "Failed to allocate memory for command map");
+		MemCache_Free(cmdbuffer);
+		return(false);
+	}
+
+	cmdmap->capacity = DEF_CMD_MAP_CAPACITY;
+	cmdmap->numcmds = 0;
+
+	cmdmap->cmds = MemCache_Alloc(sizeof(*cmdmap->cmds) * cmdmap->capacity);
+	if (!cmdmap->cmds)
+	{
+		Log_WriteSeq(LOG_ERROR, "Failed to allocate memory for command map entries");
+		MemCache_Free(cmdmap);
+		MemCache_Free(cmdbuffer);
+		return(false);
+	}
+
+	for (size_t i=0; i<cmdmap->capacity; i++)
+		cmdmap->cmds[i] = NULL;
+
+	initialized = true;
+
 	return(true);
 }
 
 void Cmd_Shutdown(void)
 {
+	if (!initialized)
+		return;
+
+	Log_WriteSeq(LOG_INFO, "Shutting down command system");
+
+	for (size_t i=0; i<cmdmap->capacity; i++)
+	{
+		cmdentry_t *current = cmdmap->cmds[i];
+		while (current)
+		{
+			cmdentry_t *next = current->next;
+			MemCache_Free(current->value);
+			MemCache_Free(current);
+			current = next;
+		}
+	}
+
+	MemCache_Free(cmdmap->cmds);
+	MemCache_Free(cmdmap);
+
+	MemCache_Free(cmdbuffer);
+
+	initialized = false;
 }
 
 void Cmd_AddCommand(const char *name, const char *description, cmdfunction_t function)
 {
+
 }
