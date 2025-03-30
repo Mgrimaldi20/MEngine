3dd0c56de8d2544c907c0de1c3c24c31ee6bd2b5
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2025-03-30 13:24:02 +1100

    this is fine

--- MEngine/src/renderer/emgl.c
+++ MEngine/src/renderer/emgl.c
@@ -4,9 +4,11 @@
 * Function: EMGL_Init
 * Loads the OpenGL library, assigns the function pointers
 * 
+* 	dllname: The name of the OpenGL library to load
+* 
 * Returns: A boolean if the initialization was successful or not
 */
-bool EMGL_Init(void)
+bool EMGL_Init(const char *dllname)
 {
 	return(true);
 }
