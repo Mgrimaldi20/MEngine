4eb42e16fa2e50f8751c22293c4483046f0dbea9
Marco Grimaldi <marco.grimaldi20@outlook.com>
2025-05-03 16:25:19 +1000

    crash handler improvements, need to get the same code to work on linux now

--- EMCrashHandler/src/emstatus.h
+++ EMCrashHandler/src/emstatus.h
@@ -1,6 +1,18 @@
 #pragma once
 
+#define EMTRACE_MAX_LINES 100
+#define EMTRACE_MAX_LINE_LEN 256
+#define EMTRACE_MAX_USERDATA_SIZE 1024
+
+typedef enum
+{
+	EMSTATUS_OK = 0,
+	EMSTATUS_ERROR = 1
+} emstatuscode_t;
+
 typedef struct
 {
-	void *placeholder;
+	emstatuscode_t status;
+	char stacktrace[EMTRACE_MAX_LINES][EMTRACE_MAX_LINE_LEN];
+	unsigned char userdata[EMTRACE_MAX_USERDATA_SIZE];
 } emstatus_t;
