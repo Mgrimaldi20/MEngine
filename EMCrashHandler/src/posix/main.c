236cdaa620e2350b91c739faa17deb75ce1ea749
Marco Grimaldi <56061871+Mgrimaldi20@users.noreply.github.com>
2025-05-03 16:25:19 +1000

    crash handler improvements, need to get the same code to work on linux now

--- EMCrashHandler/src/posix/main.c
+++ EMCrashHandler/src/posix/main.c
@@ -12,5 +12,24 @@ int main(int argc, char **argv)
 		return(1);
 	}
 
+	if (ftruncate(fd, sizeof(emstatus_t)) == -1)
+	{
+		perror("ftruncate");
+		close(fd);
+		return(1);
+	}
+
+	emstatus_t *emstatus = mmap(NULL, sizeof(emstatus_t), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (emstatus == MAP_FAILED)
+	{
+		perror("mmap");
+		close(fd);
+		return(1);
+	}
+
+	while (1)
+	{
+	}
+
 	return(0);
 }
--- EMCrashHandler/src/win32/winmain.c
+++ EMCrashHandler/src/win32/winmain.c
@@ -1,6 +1,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
+#include <shellapi.h>
+#include <stdio.h>
+#include "../emstatus.h"
+
 #pragma warning(disable: 28251)		// disables windows annotations warning
 
 static void WindowsError(void)
@@ -17,17 +21,92 @@ static void WindowsError(void)
 		NULL
 	);
 
-	MessageBox(NULL, wlpmsgbuf, L"GetLastError", MB_OK | MB_ICONINFORMATION);
+	MessageBox(NULL, wlpmsgbuf, L"EMCrashHandler: GetLastError", MB_OK | MB_ICONINFORMATION);
 	LocalFree(wlpmsgbuf);
 }
 
 int WINAPI wWinMain(HINSTANCE hinst, HINSTANCE hprevinst, PWSTR pcmdline, int ncmdshow)
 {
-	HANDLE mapfile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, L"EMCrashHandlerFileMapping");
-	if (mapfile)
+	(HINSTANCE)hinst;
+	(HINSTANCE)hprevinst;
+	(int)ncmdshow;
+
+	int argc = 0;
+	LPWSTR *argv = CommandLineToArgvW(pcmdline, &argc);	// quirky function that will not return the exe name in the string if pcmdline is not NULL
+	if (!argv)
 	{
 		WindowsError();
 		return(1);
+	}
+
+	const wchar_t *logfilename = L"crashstatus.log";
+	wchar_t logfullpath[MAX_PATH] = { 0 };
+
+	_snwprintf(logfullpath, MAX_PATH - 1, L"%s", logfilename);
+
+	if (pcmdline)
+		_snwprintf(logfullpath, MAX_PATH - 1, L"%s\\%s", argv[0], logfilename);
+
+	LocalFree(argv);	// CommandLineToArgvW allocates memory for argv
+
+	logfullpath[MAX_PATH - 1] = L'\0';
+	FILE *logfile = _wfopen(logfullpath, L"w+");
+	if (!logfile)
+	{
+		wchar_t errormsg[1024] = { 0 };
+		_snwprintf(errormsg, 1023, L"Cannot open log file: %s, directory might not exist", logfullpath);
+		errormsg[1023] = L'\0';
+
+		MessageBox(NULL, errormsg, L"EMCrashHandler: Cannot Open Log File", MB_OK | MB_ICONINFORMATION);
+		return(1);
+	}
+
+	HANDLE mapfile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, L"EMCrashHandlerFileMapping");
+	if (!mapfile)
+	{
+		WindowsError();
+		fclose(logfile);
+		return(1);
+	}
+
+	emstatus_t *emstatus = MapViewOfFile(mapfile, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(emstatus_t));
+	if (!emstatus)
+	{
+		WindowsError();
+		CloseHandle(mapfile);
+		fclose(logfile);
+		return(1);
+	}
+
+	while (1)
+	{
+		if (emstatus->status == EMSTATUS_OK)
+		{
+			fprintf(logfile, "No errors occurred during engine runtime, exiting successfully\n");
+			fflush(logfile);
+			break;
+		}
+
+		if (emstatus->status == EMSTATUS_ERROR)
+		{
+			wchar_t wlpmsgbuf[1024] = { 0 };
+			_snwprintf(wlpmsgbuf, 1023, L"An error has occurred during engine runtime, please check file [%s] for stack trace\n", logfullpath);
+			wlpmsgbuf[1023] = L'\0';
+
+			MessageBox(NULL, wlpmsgbuf, L"EMCrashHandler: ERROR", MB_OK | MB_ICONINFORMATION);
+			fwprintf(logfile, wlpmsgbuf);
+
+			for (int i=0; i<EMTRACE_MAX_LINES; i++)
+			{
+				if (emstatus->stacktrace[i][0] != '\0')
+				{
+					fprintf(logfile, "[EMSTATUS_ERROR] %s\n", emstatus->stacktrace[i]);
+					fflush(logfile);
+				}
+			}
+
+			break;
+		}
 	}
 
 	return(0);
